020e22a35f40d2471c618b8008d8aa71de2910e0
Reazier <57927915+Reazier@users.noreply.github.com>
2023-08-10 21:54:05 +0800

    update db.js

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const config = require('config');
 
 module.exports = function () {
-  const db = process.env.vidly_db;
+  const db = process.env.vidly_db_srv;
   console.log(db)
   mongoose.connect(db)
     .then(() => winston.info(`Connected to ${db}...`));
